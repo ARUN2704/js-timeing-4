65c9de27cb5d823c65922fcfa551d9f85ff2409c
ARUN2704 <arunarun45674567@gmail.com>
2023-03-24 16:55:09 +0530

    addtimeing

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+var library = [
+ { 
+ author: 'Bill Gates', 
+ title: 'The Road Ahead',
+ readingStatus: true 
+ },
+ 
+ { 
+ author: 'Steve Jobs',
+ title: 'Walter Isaacson',
+ readingStatus: true
+ 
+ },
+
+ { 
+ author: 'Suzanne Collins', 
+ title: 'Mockingjay: The Final Book of The Hunger Games',
+ readingStatus: false
+ }];
+
+var arun=library.map(function(book){
+	
+	if(book.readingStatus===true){
+	console.log(`Already read ${book.author} by the ${book.title}`)
+	
+	}
+	else{
+		console.log(`you still need to read ${book.title} by ${book.author}`)
+		
+	}
+})    
+
+
